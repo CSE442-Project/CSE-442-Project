be15a15f994027c3c4944fbd500784b659166684
vitogalv <vitogalv@buffalo.edu>
2020-02-19 20:31:00 -0500

    create client url resolves test written

--- logic/accounts/tests.py
+++ logic/accounts/tests.py
@@ -1,3 +1,16 @@
+from django.urls import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+import os
+from . import views
+from logic import settings
 
-# Create your tests here.
+host = os.getenv('HOST_NAME', None)
+if settings.DEBUG:
+    host = '127.0.0.1'
+
+class CreateClientTest(TestCase):
+
+    def test_create_client_resolves_to_create_client_view(self):
+        found = resolve('/accounts/create/client/')
+        self.assertEqual(views.create_client, found.func)
