3c2ae8dd9c37f48c9af6700d18593d9da20f180e
vitogalv <vitogalv@buffalo.edu>
2020-02-19 20:25:16 -0500

    index view servering index.html mapped to / and test written

--- logic/logic/views.py
+++ logic/logic/views.py
@@ -7,8 +7,4 @@ if settings.DEBUG:
     host = '127.0.0.1'
 
 def index(request):
-    context = {
-        'title': 'CSE 442 Project',
-        'script_src': f'http://{host}/static/bundles/main.js',
-    }
-    return render(request, 'react/react.html', context)
+    return render(request, 'logic/index.html')
