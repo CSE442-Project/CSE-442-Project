817ce5f9513da52fbcfc4bd9d4c8de83eba417a5
vitogalv <vitogalv@buffalo.edu>
2020-03-03 14:24:48 -0500

    ContractorProfile and ContractorVehicle models created to hold contractor data for #26

--- logic/accounts/models.py
+++ logic/accounts/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class ContractorProfile(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name='contractor_profile'
+    )
+    phone = models.IntegerField()
+
+    def __str__(self):
+        return self.user.username
+
+
+class ContractorVehicle(models.Model):
+    owner = models.ForeignKey(
+        ContractorProfile,
+        on_delete=models.CASCADE,
+        related_name='vehicles'
+    )
+    color = models.CharField(max_length=20)
+    plate = models.CharField(max_length=7)
+    make = models.CharField(max_length=50)
+    model = models.CharField(max_length=50)
+    year = models.IntegerField()
+
+    def __str__(self):
+        return self.plate
