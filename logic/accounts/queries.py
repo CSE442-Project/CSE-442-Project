0ccef27ab95a3add309a7db9fef920cf6afeeaaa
dorianmc99 <dorianmc@buffalo.edu>
2020-03-22 11:34:15 -0400

    I believe the getContractors function is correct, now just need to display them

--- logic/accounts/queries.py
+++ logic/accounts/queries.py
@@ -1,7 +1,20 @@
 from accounts.models import ContractorProfile, Address
 
-def getClosestContractors(){
-    currentAddi = Address.object.get(pk=1)
-    zipcode = currentAddi.zip
-    print(zipcode.ContractorProfile_set.all()[:5])
-}
+def getClosestContractors():
+    address = Address.objects.get(pk=1).values('zip')
+    contractors = ContractorProfile.objects.values('user', 'zip')
+    output = []
+    count = 0
+    for e in contractors:
+        if e["zip"] == address["zip"] && count < 5:
+            output.append(e)
+            count++
+    return output
+
+def printContractors():
+    toPrint = getClosestContractors()
+    if len(p) == 0:
+        print("Sorry, there are no available contractors in your area.")
+    else:
+        for p in toPrint:
+            print(p + '\n')
