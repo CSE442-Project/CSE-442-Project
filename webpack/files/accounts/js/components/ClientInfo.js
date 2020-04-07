e432d44769b1660af89123c301fe0f31601f75a5
dorianmc99 <dorianmc@buffalo.edu>
2020-04-06 20:14:27 -0400

    Changed file name to match task and added functionality

--- webpack/files/accounts/js/components/ClientInfo.js
+++ webpack/files/accounts/js/components/ClientInfo.js
@@ -1,36 +1,36 @@
 import React from "react";
 import Table from "react-bootstrap/Table";
 
-export default class UserInfo extends React.Component {
+export default class ClientInfo extends React.Component {
   render () {
     return (
       <div>
         <Table striped hover>
         <thead>
-        <tr>
-          <th>###</th>
-          <th>User Information</th>
-        </tr>
+          <tr>
+            <th>###</th>
+            <th>User Information</th>
+          </tr>
         <tbody>
           <tr>
             <td>Name:</td>
-            <td></td>
+            <td>{this.props.user}</td>
           </tr>
           <tr>
             <td>Address:</td>
-            <td></td>
+            <td>{this.props.address}</td>
           </tr>
           <tr>
             <td>Phone:</td>
-            <td></td>
+            <td>{this.props.phone}</td>
           </tr>
           <tr>
             <td>Size:</td>
-            <td></td>
+            <td>{this.props.dw_size}</td>
           </tr>
         </tbody>
         </Table>
       </div>
-    )
+    );
   }
 }
