6024908cac0129d9c245185b00909b826f8894b3
Sam Bond <smbond2@buffalo.edu>
2020-05-05 20:30:14 -0400

    Both clients and contractors will now receive an email stating that their account was created when they create their account.

--- logic/accounts/forms.py
+++ logic/accounts/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from . import models
 from django.contrib.auth.models import User, Group
+from django.core.mail import send_mail
+from logic.settings import EMAIL_HOST_USER
 
 
 class ClientCreationForm(forms.Form):
@@ -50,6 +52,12 @@ class ClientCreationForm(forms.Form):
         user.save()
         client_group = Group.objects.get_or_create(name='client')[0]
         user.groups.add(client_group)
+        fname = user.first_name
+        lname = user.last_name
+        subject = 'Your PlowMe Account Has Been Created'
+        message = ('Hi ' + str(fname) + ' ' + str(lname) + ', Your PlowMe client account has just been created successfully. If this was not you, please respond to this email so we can deactivate the account.')
+        recipient = user.email
+        send_mail(subject, message, EMAIL_HOST_USER, [recipient])
         return user
 
     def _create_address(self):
@@ -120,6 +128,12 @@ class ContractorCreationForm(forms.Form):
         user.save()
         contractor_group = Group.objects.get_or_create(name='contractor')[0]
         user.groups.add(contractor_group)
+        fname = user.first_name
+        lname = user.last_name
+        subject = 'Your PlowMe Account Has Been Created'
+        message = ('Hi ' + str(fname) + ' ' + str(lname) + ', Your PlowMe contractor account has just been created successfully. If this was not you, please respond to this email so we can deactivate the account.')
+        recipient = user.email
+        send_mail(subject, message, EMAIL_HOST_USER, [recipient])
         return user
 
     def create_contractor(self):
